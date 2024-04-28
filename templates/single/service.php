2a4e7a6e107cf1e3a891dff0afefaf176528a75f
kyugamer79 <kyu.gamer79@gmail.com>
2024-04-28 14:26:48 +0330

    scss variables, service.php

--- templates/single/service.php
+++ templates/single/service.php
@@ -1,9 +1,23 @@
 <?php get_header() ?>
 
 <main class="main-service">
-    <div class="clt-fix-24"></div>
 
-    
+    <div class="clr-fix-56">
+
+    </div>
+    <div class="bg-service-svg-wrapper">
+        <img class="bg-service-svg" src="<?php echo get_template_directory_uri(  ).'/assets/img/bg-service.svg'?>"
+            alt="bg-service">
+    </div>
+
+    <div class="service-hero-wrapper">
+        <div class="service-hero-txt">
+            <h2><?php the_title()?></h2>
+        </div>
+
+        <div class="sevice-hero-img"></div>
+    </div>
+
 </main>
 
 <?php get_footer() ?>
