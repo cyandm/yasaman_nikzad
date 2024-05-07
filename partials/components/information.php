131a985e6a8b6bf1e8934209deb77a479241f328
kyugamer79 <kyu.gamer79@gmail.com>
2024-05-07 11:05:27 +0330

    information responsive fixed

--- partials/components/information.php
+++ partials/components/information.php
@@ -5,26 +5,26 @@ for ($i = 1; $i <= 1; $i++) :
     $information_card = get_field("info");
 ?>
 
-    <div class="information-cta-icon-wrapper pos-relative pos-md-static">
+<div class="information-cta-icon-wrapper pos-relative">
 
-        <?php
+    <?php
 
         echo wp_get_attachment_image($information_card["info-icon"], 'full', false, ["class" => "information-cta-icon"]);
 
         ?>
-    </div>
+</div>
 
-    <div class="information-cta-txt text-natural-100 fs-h2 m-be-120">
+<div class="information-cta-txt text-natural-100 fs-h2 m-be-120">
 
-        <?php echo $information_card["info-text"] ?>
+    <?php echo $information_card["info-text"] ?>
 
-    </div>
+</div>
 
-    <div class="information-cta-btn m-be-120">
+<div class="information-cta-btn m-be-120">
 
-        <?php $info_btn = $information_card["info-btn"] ?>
+    <?php $info_btn = $information_card["info-btn"] ?>
 
-        <?php
+    <?php
 
         $btn_url = $info_btn['url'];
 
@@ -32,10 +32,11 @@ for ($i = 1; $i <= 1; $i++) :
 
         ?>
 
-        <a class="cta-btn d-flex ai-start gap-8 pi-20 pb-16 radius-12 text-natural-100 fs-body-sm" href="<?php echo esc_url($btn_url); ?>"><?php echo esc_html($btn_title); ?>
-            <img src="<?php echo get_template_directory_uri() . '/assets/img/icon-test-2-white.png' ?>">
-        </a>
+    <a class="cta-btn d-flex jc-center ai-start gap-8 pi-20 pb-16 radius-12 text-natural-100 fs-body-sm"
+        href="<?php echo esc_url($btn_url); ?>"><?php echo esc_html($btn_title); ?>
+        <img src="<?php echo get_template_directory_uri() . '/assets/img/icon-test-2-white.png' ?>">
+    </a>
 
-    </div>
+</div>
 
 <?php endfor; ?>
