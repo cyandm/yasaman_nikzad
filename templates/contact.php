12dd1849cc4481d795804d7099365256ced39e05
kyugamer79 <kyu.gamer79@gmail.com>
2024-05-11 10:28:37 +0330

    merge with fatemeh

--- templates/contact.php
+++ templates/contact.php
@@ -10,11 +10,12 @@
         <div class="col-span-4 col-span-lg-12">
             <div class="contact-us-image-wrapper d-flex jc-center ai-center h-100">
                 <img class="contact-us-image" src="<?php echo get_stylesheet_directory_uri()
-                                                        . "/assets/img/backgroundContactUs.svg" ?>" alt="design-picture">
+                                                        . "/assets/img/backgroundContactUs.svg" ?>"
+                    alt="design-picture">
             </div>
         </div>
         <div class="col-span-8 col-span-lg-12">
-            <p class="text-center fs-title text-primary-400 fs-lg-title-sm"><?php _e('Contact', 'cyn-dm') ?></p>
+            <p class="text-center fs-title text-primary-400 fs-lg-title-sm">Contact</p>
             <form class="d-grid gap-24" method="post" name="contact">
 
                 <div class="d-flex gap-24">
@@ -28,14 +29,16 @@
                     </div>
                 </div>
                 <div class="radius-16 bg-primary">
-                    <textarea name="message" id="message" cols="30" rows="10" placeholder="Message" class="p-20"></textarea>
+                    <textarea name="message" id="message" cols="30" rows="10" placeholder="Message"
+                        class="p-20"></textarea>
                 </div>
             </form>
             <div class="clr-fix-24"></div>
             <div class="d-flex f-row-reverse">
                 <button class="send-contact-us btn-secondary" type="submit">
-                    <span><?php _e('Send Message', 'cyn-dm') ?></span><img src="<?php echo get_stylesheet_directory_uri()
-                                                                                    . "/assets/img/Search.png" ?>" alt="submit-picture-contact">
+                    <span>Send Message</span><img src="<?php echo get_stylesheet_directory_uri()
+                                                            . "/assets/img/Search.png" ?>"
+                        alt="submit-picture-contact">
                 </button>
             </div>
         </div>
