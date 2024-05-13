c9bb5e6c0c6ecc2b81b24d492afa5d4a28f86e83
fateme-bakshiri <f.bakshiri@gmail.com>
2024-05-13 07:16:54 +0330

    get in touch responsive-frontPage

--- templates/frontpage.php
+++ templates/frontpage.php
@@ -5,11 +5,12 @@
 <?php $intro = get_field("intro"); ?>
 
 <main class="main">
-<!-- 
+    <!-- 
     <div class="home-page-hero-wrapper">
 
         <div class="home-page-hero-menu-icon d-none d-md-block" id="mobileMenuOpener">
-            <a href="#"><img class="pi-20 pb-16 radius-12" src="<?php //echo get_template_directory_uri() . '/assets/img/icon-test-5.png' ?>" alt="search"></a>
+            <a href="#"><img class="pi-20 pb-16 radius-12" src="<?php //echo get_template_directory_uri() . '/assets/img/icon-test-5.png' 
+                                                                ?>" alt="search"></a>
         </div>
 
         <div class="home-page-hero-swiper">
@@ -37,19 +38,22 @@
 
                 <div class="intro-img">
 
-                    <?php //echo wp_get_attachment_image($intro["intro-img"], 'full', false, ["class" => "intro-image"]); ?>
+                    <?php //echo wp_get_attachment_image($intro["intro-img"], 'full', false, ["class" => "intro-image"]); 
+                    ?>
 
                 </div>
 
                 <div class="intro-name-txt">
 
-                    <?php //echo $intro["intro-name"] ?>
+                    <?php //echo $intro["intro-name"] 
+                    ?>
 
                 </div>
 
                 <div class="intro-job-txt">
 
-                    <?php //echo $intro["intro-job"] ?>
+                    <?php //echo $intro["intro-job"] 
+                    ?>
 
                 </div>
 
@@ -59,25 +63,32 @@
 
             <div class="sidebar-menu">
 
-                <?php //if (has_nav_menu('header')) : ?>
+                <?php //if (has_nav_menu('header')) : 
+                ?>
                     <nav class="menu">
                         <?php //wp_nav_menu([
-                            //'theme_location' => 'header',
-                           // 'menu_class' => 'header-menu | d-flex f-column gap-32'
-                       // ]) ?>
+                        //'theme_location' => 'header',
+                        // 'menu_class' => 'header-menu | d-flex f-column gap-32'
+                        // ]) 
+                        ?>
                     </nav>
-                <?php //else : ?>
+                <?php //else : 
+                ?>
                     <div class=" header-no-item">
                         Please Choose a Menu
                     </div>
-                <?php //endif; ?>
+                <?php //endif; 
+                ?>
 
             </div>
 
             <div class="sidebar-social-icons">
-                <img src="<?php //echo get_template_directory_uri() . '/assets/img/icon-test-2-white.png' ?>" alt="test-icon">
-                <img src="<?php //echo get_template_directory_uri() . '/assets/img/icon-test-3-white.png' ?>" alt="test-icon">
-                <img src="<?php //echo get_template_directory_uri() . '/assets/img/icon-test-4-white.png' ?>" alt="test-icon">
+                <img src="<?php //echo get_template_directory_uri() . '/assets/img/icon-test-2-white.png' 
+                            ?>" alt="test-icon">
+                <img src="<?php //echo get_template_directory_uri() . '/assets/img/icon-test-3-white.png' 
+                            ?>" alt="test-icon">
+                <img src="<?php //echo get_template_directory_uri() . '/assets/img/icon-test-4-white.png' 
+                            ?>" alt="test-icon">
             </div>
 
         </div>
@@ -94,6 +105,10 @@
 
  -->
 
+    <section>
+
+    </section>
+
 
 
 
@@ -101,7 +116,7 @@
         <div>
             <div>
                 <p class="text-center fs-title text-primary fs-lg-title-sm pos-relative get-in-touch">Get In Touch</p>
-                <form class="d-grid gap-24  pos-relative" method="post" name="contact">
+                <form class="d-grid gap-24  pos-relative get-in-touch-form" method="post" name="contact">
                     <div class="d-flex gap-24">
                         <div class="radius-16 bg-primary flex-1 form-element">
                             <input class="p-20" type="text" placeholder="Your Name" required="required">
@@ -116,7 +131,7 @@
                 </form>
                 <div class="clr-fix-24"></div>
                 <div class="d-flex f-row-reverse">
-                    <button class="send-contact-us btn-secondary" type="submit">
+                    <button class="button btn-secondary" type="submit">
                         <span>Send Message</span><img src="<?php echo get_stylesheet_directory_uri()
                                                                 . "/assets/img/Search.png" ?>" alt="submit-picture-contact">
                     </button>
